09153b46143cb5c99502d0d1da7ab7e2c987ed0b
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-05-19 13:43:36 +0200

    format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import styles from "./App.module.css";
+import logo from "./logo.svg";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+    <div class={styles["App"]}>
+      <header class={styles["header"]}>
+        <img src={logo} class={styles["logo"]} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          class={styles.link}
+          class={styles["link"]}
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
